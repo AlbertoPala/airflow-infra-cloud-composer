d41145f8aa4383afb684b041faedad2be0a584fe
albertowo <pqdstar_alberto@live.com>
2026-01-26 22:32:30 -0600

    fix: Add serviceAccountTokenCreator role to WIF and use federated identity for storage permissions

--- terraform/modules/wif/main.tf
+++ terraform/modules/wif/main.tf
@@ -6,3 +6,12 @@ resource "google_service_account_iam_binding" "github_wif" {
     "principalSet://iam.googleapis.com/${var.wif_provider}/attribute.repository/${var.var_github_org}/${var.repo}"
   ]
 }
+
+resource "google_service_account_iam_binding" "github_token_creator" {
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
+  role               = "roles/iam.serviceAccountTokenCreator"
+
+  members = [
+    "principalSet://iam.googleapis.com/${var.wif_provider}/attribute.repository/${var.var_github_org}/${var.repo}"
+  ]
+}
