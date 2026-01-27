90de43997d5917e3f663b7c34237f2c068cdc927
albertowo <pqdstar_alberto@live.com>
2026-01-26 23:18:08 -0600

    fix: Use correct variable names in wif module (gcp_ci_sa and gcp_wif_provider)

--- terraform/modules/wif/main.tf
+++ terraform/modules/wif/main.tf
@@ -1,17 +1,17 @@
 resource "google_service_account_iam_binding" "github_wif" {
-  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.gcp_ci_sa}"
   role               = "roles/iam.workloadIdentityUser"
 
   members = [
-    "principalSet://iam.googleapis.com/${var.wif_provider}/attribute.repository/${var.var_github_org}/${var.repo}"
+    "principalSet://iam.googleapis.com/${var.gcp_wif_provider}/attribute.repository/${var.var_github_org}/${var.repo}"
   ]
 }
 
 resource "google_service_account_iam_binding" "github_token_creator" {
-  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.gcp_ci_sa}"
   role               = "roles/iam.serviceAccountTokenCreator"
 
   members = [
-    "principalSet://iam.googleapis.com/${var.wif_provider}/attribute.repository/${var.var_github_org}/${var.repo}"
+    "principalSet://iam.googleapis.com/${var.gcp_wif_provider}/attribute.repository/${var.var_github_org}/${var.repo}"
   ]
 }
