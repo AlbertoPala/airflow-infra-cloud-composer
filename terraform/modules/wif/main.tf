f68a6dc4b4a06fcc18b4963e9674b237f1185426
albertowo <pqdstar_alberto@live.com>
2026-01-30 16:49:11 -0600

    Fix WIF: replace iam_binding with iam_member to prevent permission overriding

--- terraform/modules/wif/main.tf
+++ terraform/modules/wif/main.tf
@@ -1,17 +1,13 @@
-resource "google_service_account_iam_binding" "github_wif" {
+resource "google_service_account_iam_member" "github_wif" {
   service_account_id = "projects/${var.project_id}/serviceAccounts/${var.gcp_ci_sa}"
   role               = "roles/iam.workloadIdentityUser"
 
-  members = [
-    "principalSet://iam.googleapis.com/${var.gcp_wif_pool}/attribute.repository/${var.var_github_org}/${var.repo}"
-  ]
+  member = "principalSet://iam.googleapis.com/${var.gcp_wif_pool}/attribute.repository/${var.var_github_org}/${var.repo}"
 }
 
-resource "google_service_account_iam_binding" "github_token_creator" {
+resource "google_service_account_iam_member" "github_token_creator" {
   service_account_id = "projects/${var.project_id}/serviceAccounts/${var.gcp_ci_sa}"
   role               = "roles/iam.serviceAccountTokenCreator"
 
-  members = [
-    "principalSet://iam.googleapis.com/${var.gcp_wif_pool}/attribute.repository/${var.var_github_org}/${var.repo}"
-  ]
+  member = "principalSet://iam.googleapis.com/${var.gcp_wif_pool}/attribute.repository/${var.var_github_org}/${var.repo}"
 }
